49c5a39cb3862d0c859a003754b053bf029997a6
ASC8 <asamcent@gmail.com>
2020-12-12 14:29:48 -0800

    add reply functionality

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,4 +1,5 @@
 const { Comment, Pizza } = require('../models');
+// const { db } = require('../models/Pizza');
 
 const commentController = {
     addComment({ params, body }, res) {
@@ -14,6 +15,22 @@ const commentController = {
         .then(dbPizzaData => {
             if (!dbPizzaData) {
                 res.status(404).json({ message: 'No Pizza found with this id!'});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
+    },
+
+    addReply({ params, body }, res) {
+        Comment.findOneAndUpdate(
+            {_id: params.commentId },
+            { $push: { replies: body } },
+            { new: true }
+        )
+        .then(dbPizzaData => {
+            if(!dbPizzaData) {
+                res.status(404).json({ message: 'No Pizza found with this id!' });
                 return;
             }
             res.json(dbPizzaData);
@@ -40,6 +57,17 @@ const commentController = {
             }
             res.json(dbPizzaData);
         })
+        .catch(err => res.json(err));
+    },
+
+    // remove reply
+    removeReply({ params }, res) {
+        Comment.findOneAndUpdate(
+        { _id: params.commentId },
+        { $pull: { replies: { replyId: params.replyId } } },
+        { new: true }
+        )
+        .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err => res.json(err));
     }
 };
--- models/Pizza.js
+++ models/Pizza.js
@@ -36,13 +36,13 @@ const PizzaSchema = new Schema(
     }
 );
 
-// get total count of comments and replies on retrieval 
-PizzaSchema.virtual('commentCount').get(function() {
-    return this.comments.length;
-});
-
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
+
+// get total count of comments and replies on retrieval 
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.reduce((total, comment)=> total + comment.replies.length + 1, 0);
+});
 
 // export the Pizza Model
 module.exports = Pizza;
